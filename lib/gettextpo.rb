45aafdfbe8669873fbde8a0e394ee0bb9254c856
KURASAWA Nozomu <nabetaro@caldron.jp>
2012-03-24 09:31:46 +0900

    RDoc コメントを追加

--- lib/gettextpo.rb
+++ lib/gettextpo.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2011 KURASAWA Nozomu (nabetaro)
+# Copyright (C) 2011-2012 KURASAWA Nozomu (nabetaro)
 # mailto: nabetaro@caldron.jp
 #
 # This Class describe GNU Gettext PO format file. see also:
@@ -9,10 +9,14 @@ require 'strscan'
 
 #= Class library for handling gettext po file.
 class GettextPo
+  #=== エントリ内に想定外の構造があった場合の例外
   class InvalidEntry < StandardError; end
 
-  #== One entry
+  #== GetText poファイルの1エントリを表すクラス
   class Entry
+    #=== 初期化
+    # 1エントリを表す文字列を元に、各要素を解析して格納する
+    #_lines_:: 1エントリを表す複数行の文字列
     def initialize(lines)
       s = StringScanner.new lines
       @raw = lines
@@ -75,56 +79,73 @@ class GettextPo
       end
     end
 
+    #=== ヘッダかどうか表す
     def header?
       @header_flag
     end
 
+    #=== 翻訳元文字列
     def msgid
       @msgid
     end
 
+    #=== 以前の翻訳元文字列
     def prev_msgid
       @prev_msgid
     end
 
+    #=== 翻訳文字列
     def msgstr
       @msgstr
     end
 
+    #=== 翻訳コメント
     def translator_comment
       @translator_comment
     end
 
+    #=== 抽出コメント
     def extracted_comment
       @extracted_comment
     end
 
+    #=== 参照
     def reference
       @reference
     end
 
+    #=== フラグ
     def flag
       @flag
     end
 
+    #=== エントリの生文字列
     def raw
       @raw
     end
 
+    #=== 翻訳文字列内のエスケープ文字をアンエスケープする
+    #_str_:: エスケープ文字列
     def unescape(str)
       str.gsub(/\\n/, "\n")
     end
     private :unescape
   end
 
+  #== エントリのうち先頭にあるヘッダ部
   class Header < Entry
+    #=== 初期化
+    # Entry を生成後、msgstr内の各タグを解析する
+    #_lines_:: 1エントリを表す複数行の文字列
     def initialize(lines)
       super lines
       @header_flag = true
     end
   end
 
-  # _
+  #=== 初期化する
+  #_src_:: IOクラスのインスタンス
+  #_opt_:: オプション(今は動作しない)
   def initialize(src, opt={})
     @src = src.is_a?(String) ? StringIO.new(src) : src
     @rawdata = ""
@@ -133,20 +154,31 @@ class GettextPo
     parse
   end
 
+  #=== poファイルの生データ
   attr_reader :rawdata
 
+  #=== エントリ数
+  # エントリ配列の要素数
   def size
     @entries.size
   end
 
+  #=== エントリ配列
+  # 先頭要素は GettextPo::Header のインスタンス
+  # それ以降は GettextPo::Entry のインスタンス
   def entry
     @entries
   end
 
+  #=== ヘッダエントリ
+  # GettextPo::Header のインスタンス
   def header
     @header
   end
 
+  #=== poファイルをパースする
+  # poファイル内を空行で区切り1エントリとする
+  # 先頭のエントリをヘッダとみなす
   def parse
     buf = ""
     while line = @src.gets
